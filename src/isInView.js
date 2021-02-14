4ad4f660010048b321ac6a9431eb770fcec9be66
Andy Gonzalez <zelaznogydna@gmail.com>
2021-02-14 01:47:59 -0500

    added isInView

--- src/isInView.js
+++ src/isInView.js
@@ -1,6 +1,6 @@
-const isInView = () => {
-  const shape = imageRef.current.getBoundingClientRect();
-  return shape.top >= 0 && shape.bottom <= window.innerHeight;
+const isInView = (elementRef) => {
+  const rect = elementRef.current.getBoundingClientRect();
+  return rect.top >= 0 && rect.bottom <= window.innerHeight;
 };
 
 export default isInView;
