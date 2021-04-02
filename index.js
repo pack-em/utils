066c0ed8138b54069260a3df78d50a1a2e47f2dd
Andy Gonzalez <zelaznogydna@gmail.com>
2021-04-01 20:43:46 -0400

    toTitleCase added

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import randomColor from 'randomColor';
 import scrollRefIntoView from 'scrollRefIntoView';
 import isInView from 'isInView';
 import mapIntoObject from 'mapIntoObject';
+import toTitleCase from 'toTitleCase';
 
 export { randomColor, scrollRefIntoView, isInView , mapIntoObject,toTitleCase};
